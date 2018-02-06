7a9ef8953d3ad4e33e319185f05b186068740908
Ferenc Sticza <ferenc.sticza@gmail.com>
2018-02-06 13:15:21 +0100

    section alignment

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import sprite from '../assets/img/svg/sprite.svg'
 
 const Section = ({id, title, description, children}) => {
   return (
-    <article id={id} className="section" style={{ backgroundImage: `url(${sprite}#logo-l)` }}>
+    <article id={id} className="section d-flex align-items-center" style={{ backgroundImage: `url(${sprite}#logo-l)` }}>
       <div className="container">
         {children}
       </div>
